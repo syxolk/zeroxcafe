e02de7ac995d3ef0baf653f386851b46b0b0809b
Hans <syxolk@users.noreply.github.com>
2013-10-31 08:31:53 +0000

    deleted button from activity_main.xml add documentation for MainActivity and MathUtils

--- 0xCAFE/src/com/google/code/zeroxcafe/MainActivity.java
+++ 0xCAFE/src/com/google/code/zeroxcafe/MainActivity.java
@@ -15,32 +15,53 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnItemSelectedListener {
+/**
+ * Haupt-Activity der App.
+ * 
+ * @author Hans
+ */
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		initSpinner((Spinner)findViewById(R.id.inputType));
-		initSpinner((Spinner)findViewById(R.id.outputType));
-		
-		EditText inputEdit=(EditText)findViewById(R.id.inputText);
+
+		initSpinner((Spinner) findViewById(R.id.inputType));
+		initSpinner((Spinner) findViewById(R.id.outputType));
+
+		EditText inputEdit = (EditText) findViewById(R.id.inputText);
 		inputEdit.addTextChangedListener(new TextWatcher() {
-			public void onTextChanged(CharSequence s, int start, int before, int count) {}
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
 			public void afterTextChanged(Editable s) {
 				updateValue();
 			}
 		});
 	}
-	
+
 	private void initSpinner(Spinner spinner) {
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.types, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
-		
-		spinner.setOnItemSelectedListener(this);
+
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+
+				updateValue();
+			}
+		});
 	}
 
 	@Override
@@ -49,54 +70,51 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
+	/**
+	 * Diese Methode muss aufgerufen werden, wenn sich die Eingabe-Zahl oder die
+	 * Basen geändert haben.
+	 */
 	private void updateValue() {
-		Spinner inputSpinner=(Spinner)findViewById(R.id.inputType);
-		Spinner outputSpinner=(Spinner)findViewById(R.id.outputType);
-		EditText inputEdit=(EditText)findViewById(R.id.inputText);
-		TextView outputEdit=(TextView)findViewById(R.id.outputText);
-		
-		if(inputSpinner.getSelectedItemPosition()==Spinner.INVALID_POSITION
-			|| outputSpinner.getSelectedItemPosition()==Spinner.INVALID_POSITION
-			|| inputEdit.length()==0) {
-			
+		Spinner inputSpinner = (Spinner) findViewById(R.id.inputType);
+		Spinner outputSpinner = (Spinner) findViewById(R.id.outputType);
+		EditText inputEdit = (EditText) findViewById(R.id.inputText);
+		TextView outputEdit = (TextView) findViewById(R.id.outputText);
+
+		if (inputSpinner.getSelectedItemPosition() == Spinner.INVALID_POSITION
+				|| outputSpinner.getSelectedItemPosition() == Spinner.INVALID_POSITION
+				|| inputEdit.length() == 0) {
+
 			outputEdit.setText("-");
-			
+
 		} else {
-			int baseFrom=getBaseByPos(inputSpinner.getSelectedItemPosition());
-			int baseTo=getBaseByPos(outputSpinner.getSelectedItemPosition());
-			String input=inputEdit.getText().toString().toLowerCase(Locale.US);
-			
-			if(MathUtils.isCompatible(input, baseFrom)) {
-				String output=MathUtils.convert(input, baseFrom, baseTo);
+			int baseFrom = getBaseByPos(inputSpinner.getSelectedItemPosition());
+			int baseTo = getBaseByPos(outputSpinner.getSelectedItemPosition());
+			String input = inputEdit.getText().toString()
+					.toLowerCase(Locale.US);
+
+			if (MathUtils.isCompatible(input, baseFrom)) {
+				String output = MathUtils.convert(input, baseFrom, baseTo);
 				outputEdit.setText(output);
 			} else {
-				outputEdit.setText(getResources().getString(R.string.output_error_incompatible));
+				outputEdit.setText(getResources().getString(
+						R.string.output_error_incompatible));
 			}
 		}
 	}
-	
+
 	private int getBaseByPos(int pos) {
-		switch(pos) {
-		case 0: return 2;
-		case 1: return 8;
-		case 2: return 10;
-		case 3: return 16;
-		default: return 10;
+		switch (pos) {
+		case 0:
+			return 2;
+		case 1:
+			return 8;
+		case 2:
+			return 10;
+		case 3:
+			return 16;
+		default:
+			return 10;
 		}
 	}
-	
-	public void onConvert(View view) {
-	    updateValue();
-	}
-	
-	public void onItemSelected(AdapterView<?> parent, View view,  int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-		updateValue();
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-    }
 }
--- 0xCAFE/src/com/google/code/zeroxcafe/MathUtils.java
+++ 0xCAFE/src/com/google/code/zeroxcafe/MathUtils.java
@@ -1,31 +1,60 @@
 package com.google.code.zeroxcafe;
 
 import java.math.BigInteger;
-import java.util.Locale;
 
+/**
+ * Klasse zum Umrechnen von Zahlen in andere Basen und prüfen der Kompatibilät
+ * von Zahlen auf bestimmte Basen.
+ * 
+ * @author Hans
+ */
 public class MathUtils {
+	/**
+	 * Prüft, ob eine Zahl kompatibel zu einer Basis ist, d.h. ob nur gültige
+	 * Ziffern und Zeichen aus der Basis verwendet werden.
+	 * 
+	 * @param number
+	 *            Zahl als String
+	 * @param base
+	 *            Basis
+	 * @return true, wenn kompatibel, sonst false
+	 */
 	public static boolean isCompatible(String number, int base) {
-		number=number.toLowerCase(Locale.US);
-		
-		for(int i=0; i<number.length(); i++) {
-			char ch=number.charAt(i);
-			
-			if(ch>='0' && ch<='9') {
-				if(ch-'0' >= base) {
+		for (int i = 0; i < number.length(); i++) {
+			char ch = number.charAt(i);
+
+			if (ch >= '0' && ch <= '9') {
+				if (ch - '0' >= base) {
 					return false;
 				}
-			} else if(ch>='a' && ch<='z') {
-				if(ch-'a'+10 >= base) {
+			} else if (ch >= 'a' && ch <= 'z') {
+				if (ch - 'a' + 10 >= base) {
+					return false;
+				}
+			} else if (ch >= 'A' && ch <= 'Z') {
+				if (ch - 'A' + 10 >= base) {
 					return false;
 				}
 			} else {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
+	/**
+	 * Rechnet eine Zahl von einer Basis in eine andere um. Dazu wird die
+	 * BigInteger-Klasse verwendet.
+	 * 
+	 * @param number
+	 *            Zahl als String
+	 * @param baseFrom
+	 *            altuelle Basis
+	 * @param baseTo
+	 *            Ziel-Basis
+	 * @return Zahl in der Zahl-Basis als String
+	 */
 	public static String convert(String number, int baseFrom, int baseTo) {
 		return new BigInteger(number, baseFrom).toString(baseTo);
 	}
