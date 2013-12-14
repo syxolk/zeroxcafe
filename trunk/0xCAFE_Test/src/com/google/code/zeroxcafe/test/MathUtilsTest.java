5a9c2d514fc118c979c9f3d7fd1ed00206de1193
hans.kirchner.info@gmail.com <hans.kirchner.info@gmail.com@097ca3fb-27e0-0d2c-02bc-488023a687c9>
2013-12-14 17:36:46 +0000

    check in 0xCAFE test app

---
+++ trunk/0xCAFE_Test/src/com/google/code/zeroxcafe/test/MathUtilsTest.java
@@ -0,0 +1,80 @@
+package com.google.code.zeroxcafe.test;
+
+import static com.google.code.zeroxcafe.MathUtils.convert;
+import static com.google.code.zeroxcafe.MathUtils.hasMaximumOneDecimalPoint;
+import static com.google.code.zeroxcafe.MathUtils.isCompatible;
+import junit.framework.TestCase;
+
+/**
+ * TestCases for {@link com.google.code.zeroxcafe.MathUtils}.
+ * 
+ * @author Hans
+ */
+public class MathUtilsTest extends TestCase {
+
+	public void testIsCompatible() {
+		assertTrue(isCompatible("01", 2));
+		assertTrue(isCompatible("0.1", 2));
+		assertFalse(isCompatible("-0.1", 2));
+		assertFalse(isCompatible("0.12", 2));
+	}
+
+	public void testHasMaximumOneDecimalPoint() {
+		assertTrue(hasMaximumOneDecimalPoint("123.1"));
+		assertTrue(hasMaximumOneDecimalPoint("AB.C"));
+		assertFalse(hasMaximumOneDecimalPoint("AB..C"));
+		assertFalse(hasMaximumOneDecimalPoint("ABC.."));
+		assertFalse(hasMaximumOneDecimalPoint("..ABC"));
+		assertFalse(hasMaximumOneDecimalPoint(".ABC."));
+	}
+
+	public void testConvert() {
+		assertEquals("10011010011", convert("10011010011", 2, 2));
+		assertEquals("2323", convert("10011010011", 2, 8));
+		assertEquals("1235", convert("10011010011", 2, 10));
+		assertEquals("4D3", convert("10011010011", 2, 16));
+
+		assertEquals("10011010010", convert("2322", 8, 2));
+		assertEquals("2322", convert("2322", 8, 8));
+		assertEquals("1234", convert("2322", 8, 10));
+		assertEquals("4D2", convert("2322", 8, 16));
+
+		assertEquals("10011010010", convert("1234", 10, 2));
+		assertEquals("2322", convert("1234", 10, 8));
+		assertEquals("1234", convert("1234", 10, 10));
+		assertEquals("4D2", convert("1234", 10, 16));
+
+		assertEquals("1100101011111110", convert("CAFE", 16, 2));
+		assertEquals("145376", convert("CAFE", 16, 8));
+		assertEquals("51966", convert("CAFE", 16, 10));
+		assertEquals("CAFE", convert("CAFE", 16, 16));
+	}
+
+	public void testConvertBorderCases() {
+		assertEquals("0", convert(".", 10, 2));
+		assertEquals("0", convert("0.", 10, 2));
+		assertEquals("0", convert(".0", 10, 2));
+	}
+
+	public void testConvertWithDecimalPoint() {
+		assertEquals("10011010011.101", convert("10011010011.1010", 2, 2));
+		assertEquals("2323.5", convert("10011010011.1010", 2, 8));
+		assertEquals("1235.625", convert("10011010011.1010", 2, 10));
+		assertEquals("4D3.A", convert("10011010011.1010", 2, 16));
+
+		assertEquals("1010011100.101", convert("1234.5", 8, 2));
+		assertEquals("1234.5", convert("1234.5", 8, 8));
+		assertEquals("668.625", convert("1234.5", 8, 10));
+		assertEquals("29C.A", convert("1234.5", 8, 16));
+		
+		assertEquals("10011010010.1", convert("1234.5", 10, 2));
+		assertEquals("2322.4", convert("1234.5", 10, 8));
+		assertEquals("1234.5", convert("1234.5", 10, 10));
+		assertEquals("4D2.8", convert("1234.5", 10, 16));
+		
+		assertEquals("10100001001000110100.01011011", convert("A1234.5B", 16, 2));
+		assertEquals("2411064.266", convert("A1234.5B", 16, 8));
+		assertEquals("660020.35546875", convert("A1234.5B", 16, 10));
+		assertEquals("A1234.5B", convert("A1234.5B", 16, 16));
+	}
+}
